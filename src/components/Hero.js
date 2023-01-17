55113eb06485341450cca6c21e219602fa23885e
Anuj <anujk.581@gmail.com>
2023-01-17 02:45:55 -0500

    finished Hero page and added pictures

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
 export default function Hero() {
+
+    var heroBackground = require("../images/heroBackground.png");
+    var pose = require("../images/pose1.png");
+
+
   return (
-    <div className=' bg-slate-700 h-[90vh] flex justify-center justify-items-center'>
-    Hero section
+    <div className=' bg-white h-[93vh] grid grid-cols-2 auto-cols-auto px-5 py-5'>
+    <div className="left flex flex-col">
+    
+    <h3 className=' text-2xl py-5 px-4 mt-32 text-[#C47A5E]'>NAMASTE </h3>
+    <h2 className='font-serif text-4xl py-5 px-4 text-[#513125]' style={{}}>BEND YOUR MIND,<br/> INSPIRE YOURSELF</h2>
+    <p className=' py-5 px-4 text-[#7B7B7B] text-2xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.  Blanditiis enim tenetur dolor tempore nam in fugiat illum itaque quam, sunt, quo soluta totam, sed aut. Possimus, corrupti ad. Quis, perspiciatis?</p>
+    
+    <div className="buttons py-5 px-4 pb-3 mt-5 flex justify-around">
+    <button type="button" class="focus:outline-none text-white bg-[#C47A5E] hover:border-2 border-[#513125] hover:text-[#513125] focus:ring-4 focus:ring-yellow-300 font-medium  text-base px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Learn more</button>
+    <button type="button" class=" text-[#513125] bg-transparent hover:bg-[#C47A5E]   font-medium  text-base px-5 py-1.5 mr-2 mb-2  border-2 border-[#513125] focus:ring-4 focus:ring-yellow-300">Contact Us</button></div>
+
+    </div>
+    <div className="right flex justify-center items-center bg-no-repeat bg-cover bg-center  " style={{backgroundImage: `url(${heroBackground})`}}>
+    
+    <img src={pose} alt="pic" style={{width:"80%"}} />
+    
+    </div>
     
 
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,25 +4,23 @@ export default function Navbar() {
   var logo = require("../images/logo.png");
 
   return (
-    <div className=" text-amber-900 bg-orange-100 w-full flex items-center px-4 py-2 ">
-      <img src={logo} alt="logo" style={{ width: "50px", height: "50px" }} />
+    <div className=" text-[#513125] bg-orange-100 w-full font-bold flex items-center px-4 py-2 ">
+      <img src={logo} alt="logo" style={{ width: "50px", height: "50px" }} className="mx-1" />
       <div className=" flex justify-between w-full">
         <h1 className=" font-bold text-3xl mx-1">Yoga Roots</h1>
         <ul className=" flex items-center">
-          <li className=" px-4 mx-1 hover: cursor-pointer hover:font-bold ">
+          <li className=" px-4 mx-1 hover: cursor-pointer hover:text-xl ">
             Home
           </li>
-          <li className=" px-4 mx-1 hover: cursor-pointer hover:font-bold ">
+          <li className=" px-4 mx-1 hover: cursor-pointer hover:text-xl ">
             About Us
           </li>
-          <li className=" px-4 mx-1 hover: cursor-pointer hover:font-bold">
+          <li className=" px-4 mx-1 hover: cursor-pointer hover:text-xl">
             Contact
           </li>
-          <li className=" px-4 mx-1 hover: cursor-pointer hover:font-bold">
+          <li className=" px-4 mx-1 hover: cursor-pointer hover:text-xl">
             Pricing
           </li>
         </ul>
       </div>
-    </div>
-  );
-}
+    </div>)}
