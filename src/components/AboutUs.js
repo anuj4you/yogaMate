07ca01b0f435ac2bfcdb23120ba144aea30e96a9
Anuj <anujk.581@gmail.com>
2023-01-17 19:39:40 -0500

    added aboutUs and fixed some styles

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -2,55 +2,56 @@ import React from "react";
 
 export default function AboutUs() {
   return (
-    <div>
-      <h4>About Us </h4>
-      <h5>IT'S ALL YOGA, ALL THE TIME</h5>
+    <div className=" h-[95vh] flex flex-col justify-center justify-items-center text-center">
+      <h4 className="font-bold text-2xl py-1 px-4 text-[#C47A5E]">About Us </h4> <hr />
+      <h5 className="cormorant text-4xl py-1 px-4 text-[#513125]">IT'S ALL YOGA, ALL THE TIME</h5>
 
-      <div className=" flex items-center justify-center text-center grid mb-8  shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2 px-4 py-4 ">
-        <div className="flex flex-col items-center justify-center p-8 text-center bg-[#C47A5E] px-3 py-2 mx-3 my-3 w-7/12 ">
-          <blockquote className="max-w-2xl mx-auto mb-4 text-[#D9D9D9] lg:mb-8 dark:text-gray-400">
+      <div className=" grid grid-cols-2 gap-4 justify-items-center px-4 py-4">
+        <div className=" h-64 flex flex-col items-center justify-center text-center bg-[#C47A5E] px-3 py-2 mx-3 my-3 w-7/12 ">
+          
             <h3 className="text-2xl font-semibold text-white dark:text-white  ">
               COMFORT PLACE 
             </h3>
-            <p className="my-4 font-light">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor repellat eius autem.
+            <p className="my-4 font-light text-[#D9D9D9]">
+            LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT. NEGAT ESSE EAM, INQUIT, PROPTER SE EXPETENDAM. AT HOC IN EO M. SI VERBUM
             </p>
-          </blockquote>
+          
           
         </div>
-        <div className="flex flex-col items-center justify-center p-8 text-center bg-[#C47A5E] px-3 py-2 mx-3 my-3 w-7/12 ">
-          <blockquote className="max-w-2xl mx-auto mb-4 text-[#D9D9D9] lg:mb-8 dark:text-gray-400">
+        <div className=" h-64 flex flex-col items-center justify-center text-center bg-[#513125] px-3 py-2 mx-3 my-3 w-7/12 ">
+          
             <h3 className="text-2xl font-semibold text-white dark:text-white  ">
-              COMFORT PLACE 
+              EXPERT TRAINER
+            </h3>
+            <p className="my-4 font-light text-[#D9D9D9] ">
+            LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT. NEGAT ESSE EAM, INQUIT, PROPTER SE EXPETENDAM. AT HOC IN EO M. SI VERBUM
+            </p>
+          
+          
+        </div>
+        <div className=" h-64 flex flex-col items-center justify-center text-center bg-[#FFEFD1] px-3 py-2 mx-3 my-3 w-7/12 ">
+          
+            <h3 className="text-2xl font-semibold text-[#513125] dark:text-white  ">
+              FRIENDLY PLACE 
+            </h3>
+            <p className="my-4 font-light text-[#936351] ">
+            LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT. NEGAT ESSE EAM, INQUIT, PROPTER SE EXPETENDAM. AT HOC IN EO M. SI VERBUM
+            </p>
+          
+          
+        </div>
+        <div className=" h-64 flex flex-col items-center justify-center text-center bg-[#C47A5E] px-3 py-2 mx-3 my-3 w-7/12 ">
+          
+            <h3 className="text-2xl font-semibold text-white dark:text-white  ">
+              BEST REVIEWS 
             </h3>
             <p className="my-4 font-light">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor repellat eius autem.
+            LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT. NEGAT ESSE EAM, INQUIT, PROPTER SE EXPETENDAM. AT HOC IN EO M. SI VERBUM
             </p>
-          </blockquote>
+          
           
         </div>
-        <div className="flex flex-col items-center justify-center p-8 text-center bg-[#C47A5E] px-3 py-2 mx-3 my-3 w-7/12 ">
-          <blockquote className="max-w-2xl mx-auto mb-4 text-[#D9D9D9] lg:mb-8 dark:text-gray-400">
-            <h3 className="text-2xl font-semibold text-white dark:text-white  ">
-              COMFORT PLACE 
-            </h3>
-            <p className="my-4 font-light">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor repellat eius autem.
-            </p>
-          </blockquote>
-          
-        </div>
-        <div className="flex flex-col items-center justify-center p-8 text-center bg-[#C47A5E] px-3 py-2 mx-3 my-3 w-7/12 ">
-          <blockquote className="max-w-2xl mx-auto mb-4 text-[#D9D9D9] lg:mb-8 dark:text-gray-400">
-            <h3 className="text-2xl font-semibold text-white dark:text-white  ">
-              COMFORT PLACE 
-            </h3>
-            <p className="my-4 font-light">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor repellat eius autem.
-            </p>
-          </blockquote>
-          
-        </div>
+
 
       </div>
     </div>
--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -2,22 +2,23 @@ import React from "react";
 
 export default function Benefits() {
 
+    var benefitBg = require("../images/benefitBg.png");
     var pose8 = require("../images/pose8.png");
     var pose3 = require("../images/pose3.png");
     var pose5 = require("../images/pose5.png");
     var pose2 = require("../images/pose2.png");
 
   return (
-    <div className=" bg-orange-50 h-[75vh] px-5 py-5 flex flex-col justify-center  ">
-      <h3 className=" text-[#513125] text-4xl flex justify-center font-bold cormorant py-3 my-2">
+    <div className=" bg-orange-50 h-[100vh] px-5 py-3 flex flex-col justify-center bg-no-repeat bg-cover bg-center " style={{backgroundImage: `url(${benefitBg})`}}>
+      <h3 className=" text-[#513125] text-4xl flex justify-center font-bold cormorant py-1 my-1">
         Benefits of yoga
       </h3>
-      <p className="text-lg text-center text-[#4C4C4C] px-32 my-3 w-2/3 mx-auto">
+      <p className="text-lg text-center text-[#4C4C4C] px-32 my-1 w-2/3 mx-auto">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita eius
         est error, nulla quia quae harum ipsa, alias quas architecto aliquam{" "}
       </p>
 
-      <div className="cards flex flex-wrap justify-evenly my-4 mx-3 ">
+      <div className="cards flex flex-wrap justify-evenly my-2 mx-3 ">
         <div className=" my-3 flex flex-col justify-center text-center shadow-md  max-w-sm p-4 bg-white border border-gray-200 rounded-lg  hover:bg-[#ceb1a6] dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
           
         <img src={pose8} alt="pic" style={{width:"27%", margin:"auto"}} />
